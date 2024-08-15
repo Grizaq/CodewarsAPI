54fae6de205458cacea1840ec123beaeef8caf7c
grizaq <77796055+Grizaq@users.noreply.github.com>
2024-08-15 15:42:18 +0100

    checking token on new name

--- app/src/main/java/com/example/tuicodewars/presentation/MainActivity.kt
+++ app/src/main/java/com/example/tuicodewars/presentation/MainActivity.kt
@@ -14,6 +14,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             TuiCodewarsTheme {
                 DestinationsNavHost(navGraph = NavGraphs.root)
+                // for testing
             }
         }
     }
